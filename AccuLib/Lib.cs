f9098e66e240a4662e821430d354242fdb3de721
User <User@DESKTOP-003QCKM>
2022-08-15 15:15:20 +0700

    Add project files.

---
+++ AccuLib/Lib.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccuLib
+{
+    public class Lib
+    {
+        public static string getName()
+        {
+            return "welcome to my world";
+        }
+
+    }
+}
